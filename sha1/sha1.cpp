c5b8d227fe664d8d42cc0423444b2bc9c7b873ba
shamilkvv <shamiltv803@gmail.com>
2023-12-21 06:38:43 +0400

    Добавьте файлы проекта.

---
+++ sha1/sha1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+template <typename T>
+
+T leftСyclicShift(T value, int positions) {
+    const int bitSize = sizeof(T) * 8;
+    return (value << positions) | (value >> (bitSize - positions));
+}
+
+
+int main() {
+   
+
+    return 0;
+}
