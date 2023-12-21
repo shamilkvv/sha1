3ef9e69ca62e827c6028cb4359b2248dc5c1b942
shamilkvv <shamiltv803@gmail.com>
2023-12-21 06:44:38 +0400

    add nonLinFunc function

--- sha1/sha1.cpp
+++ sha1/sha1.cpp
@@ -7,6 +7,22 @@ T leftСyclicShift(T value, int positions) {
     return (value << positions) | (value >> (bitSize - positions));
 }
 
+unsigned int nonLinFunc(int i, unsigned int B, unsigned int C, unsigned int D) {
+    if (0 <= i && i <= 19) {
+        return (B & C) | ((~B) & D);
+    }
+    else if (20 <= i && i <= 39) {
+        return B ^ C ^ D;
+    }
+    else if (40 <= i && i <= 59) {
+        return (B & C) | (B & D) | (C & D);
+    }
+    else if (60 <= i && i <= 79) {
+        return B ^ C ^ D;
+    }
+    return 0;
+}
+
 int main() {
    
 
